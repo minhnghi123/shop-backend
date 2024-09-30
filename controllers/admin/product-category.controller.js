6e6c6748e55cb053c60294b3aa8e5c77a7335855
Alien <minhhnghiii@gmail.com>
2024-09-30 18:17:16 +0700

    edit products-category

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -27,3 +27,28 @@ module.exports.createPost = async (req, res) => {
   await record.save();
   res.redirect(`/${systemConfig.prefixAdmin}/products-category`);
 };
+module.exports.edit = async (req, res) => {
+  const categoryId = req.params.id;
+  // console.log(categoryId);
+  const category = await ProductCategory.findOne({ _id: categoryId });
+  // console.log(category);
+  const listCategory = await ProductCategory.find({
+    deleted: false,
+  });
+  res.render("admin/pages/products-category/edit", {
+    pageTitle: "Chỉnh danh mục sản phẩm",
+    listCategory: listCategory,
+    category,
+  });
+};
+module.exports.editPatch = async (req, res) => {
+  // console.log(req.body);
+  const categoryId = req.params.id;
+  req.body.position = parseInt(req.body.position);
+  await ProductCategory.updateOne(
+    { _id: categoryId, deleted: false },
+    req.body
+  );
+  req.flash("success", "Cập nhật thành công!");
+  res.redirect("back");
+};
--- routes/admin/product-category.route.js
+++ routes/admin/product-category.route.js
@@ -12,4 +12,11 @@ router.post(
   uploadCloud.uploadCloudSingle,
   controller.createPost
 );
+router.get("/edit/:id", controller.edit);
+router.patch(
+  "/edit/:id",
+  upload.single("thumbnail"),
+  uploadCloud.uploadCloudSingle,
+  controller.editPatch
+);
 module.exports = router;
