b077f2030cee488081fd230b091b86f4f9928397
Alien <minhhnghiii@gmail.com>
2024-10-01 19:47:24 +0700

    creating role collection

---
+++ models/role.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const RoleSchema = new mongoose.Schema(
+  {
+    title: String,
+    description: String,
+    permissions: {
+      type: Array,
+      default: [],
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Role = mongoose.Model("Role", RoleSchema, "roles");
+module.exports = Role;
